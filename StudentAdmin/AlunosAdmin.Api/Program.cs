9a6cd56b3231ae53e602acb68488994ef653ca96
RaffDevs <rveronezdeveloper@gmail.com>
2024-07-29 18:00:20 -0300

    feat: conf entity framework core drive with postgres.

--- StudentAdmin/AlunosAdmin.Api/Program.cs
+++ StudentAdmin/AlunosAdmin.Api/Program.cs
@@ -1,7 +1,9 @@
+using AlunosAdmin.CrossCutting.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDatabaseContext(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
