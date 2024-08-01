ec31e102e5f303fc53f18e581e830df168407e12
RaffDevs <rveronezdeveloper@gmail.com>
2024-08-01 12:43:22 -0300

    feat: create web client project

---
+++ student-admin-client/src/components/DrawerComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-list dense>
+    <v-list-item
+        link
+        @click="navigateToAlunos"
+    >
+      <v-list-item-icon>
+        <v-icon>mdi-account-group</v-icon>
+      </v-list-item-icon>
+      <v-list-item-content >
+        <v-list-item-title>Alunos</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+
+<script>
+export default {
+  name: "DrawerComponent",
+  data: () => ({
+    items: [
+      {title: 'Dashboard', icon: 'mdi-view-dashboard'},
+      {title: 'Photos', icon: 'mdi-image'},
+      {title: 'About', icon: 'mdi-help-box'},
+    ],
+    right: null,
+  })
+
+}
+</script>
+
+
+<style scoped>
+
+</style>
