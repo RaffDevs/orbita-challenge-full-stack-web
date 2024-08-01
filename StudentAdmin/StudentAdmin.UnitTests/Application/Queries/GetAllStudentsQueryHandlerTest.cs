68f150d12c15a33d8a18fdaf0c250d4779762108
RaffDevs <rveronezdeveloper@gmail.com>
2024-08-01 07:43:43 -0300

    test: test GetStudentByIdQueryHandler class

--- StudentAdmin/StudentAdmin.UnitTests/Application/Queries/GetAllStudentsQueryHandlerTest.cs
+++ StudentAdmin/StudentAdmin.UnitTests/Application/Queries/GetAllStudentsQueryHandlerTest.cs
@@ -33,24 +33,5 @@ public class GetAllStudentsQueryHandlerTest
         Assert.Equal(students.Count, studentViewModelList.Count);
     }
     
-    [Fact]
-    public async Task ExistsTwoStudentWithId_GetById_ReturnStudentWithSameId()
-    {
-        // Arrange
-        var student = new Student("123345456", "Rafael Veronez", "teste@mail.com", "317.723.630-57");
-        
-        var studentRepositoryMock = new Mock<IStudentRepository>();
-        var studentId = "317.723.630-57";
-        studentRepositoryMock.Setup(st => st.GetByIdAsync(studentId).Result)
-            .Returns(student);
-        var query = new GetStudentByIdQuery(studentId);
-        var getStudentByIdQueryHandler = new GetStudentByIdQueryHandler(studentRepositoryMock.Object);
-
-        // Act
-        var studentViewModel = await getStudentByIdQueryHandler.Handle(query, new CancellationToken());
-        
-        // Assert
-        Assert.NotNull(studentViewModel);
-        Assert.Equal(student.Cpf, studentViewModel.Cpf);
-    }
+    
 }
---
+++ StudentAdmin/StudentAdmin.UnitTests/Application/Queries/GetStudentByIdQueryHandlerTest.cs
@@ -0,0 +1,52 @@
+using Moq;
+using StudentAdmin.Application.Exceptions;
+using StudentAdmin.Application.Queries.GetStudentById;
+using StudentAdmin.Core.Entities;
+using StudentAdmin.Core.Repositories;
+
+namespace StudentAdmin.UnitTests.Application.Queries;
+
+public class GetStudentByIdQueryHandlerTest
+{
+    [Fact]
+    public async Task ExistsOneStudentWithId_GetById_ReturnStudentWithSameId()
+    {
+        // Arrange
+        var student = new Student("123345456", "Rafael Veronez", "teste@mail.com", "317.723.630-57");
+
+        var studentRepositoryMock = new Mock<IStudentRepository>();
+        const string studentId = "317.723.630-58";
+        studentRepositoryMock.Setup(st => st.GetByIdAsync(studentId).Result)
+            .Throws<NotFoundStudentException>(() => new NotFoundStudentException());
+        var query = new GetStudentByIdQuery(studentId);
+        var getStudentByIdQueryHandler = new GetStudentByIdQueryHandler(studentRepositoryMock.Object);
+
+        // Act
+        var exception = await Assert.ThrowsAsync<NotFoundStudentException>(() =>
+            getStudentByIdQueryHandler.Handle(query, new CancellationToken()));
+
+        // Assert
+        Assert.Equal(new NotFoundStudentException().Message, exception.Message);
+    }
+
+    [Fact]
+    public async Task ExistsOneStudentWithAnotherId_GetById_ThrowsNotFoundStudentException()
+    {
+        // Arrange
+        var student = new Student("123345456", "Rafael Veronez", "teste@mail.com", "317.723.630-57");
+
+        var studentRepositoryMock = new Mock<IStudentRepository>();
+        var studentId = "317.723.630-57";
+        studentRepositoryMock.Setup(st => st.GetByIdAsync(studentId).Result)
+            .Returns(student);
+        var query = new GetStudentByIdQuery(studentId);
+        var getStudentByIdQueryHandler = new GetStudentByIdQueryHandler(studentRepositoryMock.Object);
+
+        // Act
+        var studentViewModel = await getStudentByIdQueryHandler.Handle(query, new CancellationToken());
+
+        // Assert
+        Assert.NotNull(studentViewModel);
+        Assert.Equal(student.Cpf, studentViewModel.Cpf);
+    }
+}
